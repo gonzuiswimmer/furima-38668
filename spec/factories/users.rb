d95ae9d3027a4cbb9d33872aa433ffb6ac6cbc09
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-09 18:19:23 +0900

    テストコードの下書き
    （password,password_confirmationのFactoryBotが機能していない）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {encrypted_password}
+    nickname {Faker::Name.name}
+    last_name {Faker::Name.last_name}
+    first_name {Faker::Name.first_name}
+    last_name_kana {Gimei.last.katakana}
+    first_name_kana {Gimei.first.katakana}
+    birth_date {Faker::Date.birthday(min_age: 18, max_age: 65)}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー情報が登録できる' do
+    it "すべての情報が入力されており、登録・保存ができる" do
+      expect(@user).to be_valid
+    end
+  end
+
+  context 'ユーザー情報が登録できない' do
+    it "" do
+      
+    end
+    it "" do
+      
+    end
+    it "" do
+      
+    end
+    it "" do
+      
+    end
+    it "" do
+      
+    end
+    it "" do
+      
+    end
+    it "" do
+      
+    end
+  end
+
+
 end
