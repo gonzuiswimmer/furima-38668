80d1c1654d54d9790e74e5f701e7d1bd57e75f16
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-11 17:56:19 +0900

    activehashを用いたモデリングと設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::AtiveRecordExtensions
+
+
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :required_date
+  belongs_to :shipping_charge
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,5 @@ class User < ApplicationRecord
   validates :first_name_kana,presence: true, format:{with: /\A[ァ-ヶー－]+\z/ , message: "is invalid. Input half-width characters."}
   validates :birth_date, presence: true
 
-
+  has_many :items
 end
