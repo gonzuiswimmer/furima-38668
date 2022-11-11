15d53623413670704ffeeb4b10d09097e57bb433
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-11 18:21:25 +0900

    active_storageを使用したimageテーブルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::AtiveRecordExtensions
 
-
   belongs_to :user
+  has_one_attached :image
   belongs_to :category
   belongs_to :condition
   belongs_to :prefecture
