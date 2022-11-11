b5705944a86fc9d5b46f8a9d8d48203904110a95
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-11 17:09:40 +0900

    商品出品ページへのルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,13 @@ class ItemsController < ApplicationController
   def index
   end
   
+  def new
+    @item = Item.new
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render: :new
+    end    
+  end
 end
