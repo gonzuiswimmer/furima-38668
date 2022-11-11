2ce30efdca52b3b3c66c460b8c45c2fe0ce79b29
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-11 14:55:09 +0900

    ログインにおけるエラーメッセージの表示の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name,presence: true, format:{with: /\A[一-龥]+\z/ , message:"is invalid. Input full-width characters."}
+  validates :first_name,presence: true, format:{with: /\A[一-龥]+\z/, message:"is invalid. Input full-width characters."}
+  validates :last_name_kana,presence: true, format:{with: /\A[ァ-ヶー－]+\z/ , message: "is invalid. Input half-width characters."} 
+  validates :first_name_kana,presence: true, format:{with: /\A[ァ-ヶー－]+\z/ , message: "is invalid. Input half-width characters."}
+  validates :birth_date, presence: true
+
+  has_many :items
 end
