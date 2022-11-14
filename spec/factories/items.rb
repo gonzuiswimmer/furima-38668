9e79855292da747b00d4f475a643ed541bea377c
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-14 13:54:20 +0900

    商品出品単体テストコードの条件の洗い出し

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {}
+    details {}
+    category_id {}
+    condition_id {}
+    shipping_charge_id {}
+    prefecture_id {}
+    required_date_id {}
+    price {}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item)
+  end
+
+  context '商品が出品でき、保存される' do
+    it 'すべての情報が入力されている' do
+      expect(@item).to be_valid
+    end
+  end
+
+  context '商品情報が保存されない' do
+    it 'imageが空では保存できない' do
+    end
+    it 'image_nameが空では保存できない' do
+    end
+    it 'detailsが空では保存できない' do
+    end
+    it 'category_idが未選択では保存できない' do
+    end
+    it 'prefecture_idが未選択では保存できない' do
+    end
+    it 'required_date_idが未選択では保存できない' do
+    end
+    it 'shipping_charge_idが未選択では保存できない' do
+    end
+    it 'priceが未選択では保存できない' do
+    end
+    it 'priceが300円未満では保存できない' do
+    end
+    it 'priceが10,000,000円以上では保存できない' do
+    end
+    it 'priceが半角数字でないと保存できない' do
+    end
+
+  end
+
 end
