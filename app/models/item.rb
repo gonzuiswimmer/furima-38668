8f339565ac46f8a3a38a3c2a1420b27d76205a37
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-14 18:18:47 +0900

    表記の修正（rubocop）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  with_options presence: true do 
+  with_options presence: true do
     validates :image
-    validates :item_name, length:{minimum:1, maximum: 40}
-    validates :details, length:{minimum:1, maximum: 1000}
-    validates :category_id, numericality:{other_than:0, message:"is invalid"}
-    validates :prefecture_id, numericality:{other_than:0, message:"is invalid"}
-    validates :required_date_id, numericality:{other_than:0, message:"is invalid"}
-    validates :shipping_charge_id, numericality:{other_than:0, message:"is invalid"}
-    validates :price, presence: true, numericality: {greater_than_or_equal_to:300, less_than_or_equal_to:9999999, only_integer:true, message:"is invalid(¥300~9,999,999)"}
+    validates :item_name, length: { minimum:1, maximum: 40 }
+    validates :details, length: { minimum:1, maximum: 1000 }
+    validates :category_id, numericality: { other_than:0, message: 'is invalid' }
+    validates :prefecture_id, numericality: { other_than:0, message: 'is invalid' }
+    validates :required_date_id, numericality: { other_than:0, message: 'is invalid' }
+    validates :shipping_charge_id, numericality: { other_than:0, message: 'is invalid' }
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer:true, message: 'is invalid(¥300~9,999,999)' }
   end
 
   belongs_to :user
