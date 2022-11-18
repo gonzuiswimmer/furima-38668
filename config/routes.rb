018ef3412e6857d2fc5c7ba2c9b2a5a6d86f0317
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-18 14:48:08 +0900

    ordersコントローラーのネストの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :users
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :corders, only: [:index, :create]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :orders, only: [:index, :create]
+  end
+  
 end
