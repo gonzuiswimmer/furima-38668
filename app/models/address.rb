6f865f1b9bdb8cc65a0227ecd207c4aea0a6c8c5
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-18 14:25:15 +0900

    バリデーションの追記

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,14 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
+  with_options presence: true do
+    validates: :post_number
+    validates: :prefecture_id, numericality: {other_than: 0, message: 'is invalid'}
+    validates: :city_town
+    validates: :house_number
+    validates: :phone_number
+  end
+
   belongs_to :purchased_record
   belongs_to :prefecture
 end
